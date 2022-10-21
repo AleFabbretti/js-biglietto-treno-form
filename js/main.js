fd742e9b75a168303e40e3ef978ed7c711de5842
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-21 17:54:20 +0200

    Aggiunte condizioni per il ticket da assegnare

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 const btnGenerator = document.querySelector(".btn-generator");
 
 btnGenerator.addEventListener(`click`, function() {
-    //1 chiedi all'utente il nome e il cognome
+    // chiedi all'utente il nome e il cognome
     let nameSurname = document.getElementById("name").value;
 
-    //2 chiedi all'utente il numero dei km che vuole percorrere
-    let distance = Number( document.getElementById(km).value);
+    // chiedi all'utente il numero dei km che vuole percorrere
+    let km = Number( document.getElementById("km").value);
     
-    //3 chiedi all'utente l'età del passeggero
-    let age = document.getElementById(age).value;
+    // chiedi all'utente l'età del passeggero
+    let age = document.getElementById("age").value;
 
+    console.log("nameSurname, distance, age");
+
+    // crea una variabile in base al prezzo al km (pari a 0.21€)
+    let price = km * 0.21;
+
+    // crea delle condizioni per applicare lo sconto in base all'età dell'utente
+    if (age === "minorenne") {
+        price -= price * 0.20;
+        message = "Biglietto per minorenni" 
+    } else if (age === "over-65") {
+        price -= price * 0.40;
+        message = "Biglietto per over 65"
+    } else {
+        price = km * 0.21;
+        message = "Biglietto per Adulti"
+    }
+
+    // stampo prezzo biglietto con eventuale scontistica applicata
+    console.log(`${price.toFixed(2)}€`);
+    console.log(message);
 })
