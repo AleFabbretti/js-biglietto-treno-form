1877cba157580cbedf3d407df74bbd7b9c7a293e
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-21 17:31:25 +0200

    Aggiunte tre variabili principali

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 const btnGenerator = document.querySelector(".btn-generator");
+
+btnGenerator.addEventListener(`click`, function() {
+    //1 chiedi all'utente il nome e il cognome
+    let nameSurname = document.getElementById("name").value;
+
+    //2 chiedi all'utente il numero dei km che vuole percorrere
+    let distance = Number( document.getElementById(km).value);
+    
+    //3 chiedi all'utente l'età del passeggero
+    let age = document.getElementById(age).value;
+
+})
